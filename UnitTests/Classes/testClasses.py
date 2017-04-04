ed9d707c2b8dcb7a81105d2ea501e9e6847977f0
Reid <reid.vollett@uoit.net>
2017-04-04 16:15:04 -0400

    Unit test

---
+++ UnitTests/Classes/testClasses.py
@@ -0,0 +1,130 @@
+import unittest
+
+class Alien(unittest.TestCase):
+    def setUp(self):
+        print("Setup\n")
+        #self.X = X
+
+    def tearDown(self):
+        print("Takedown\n")
+        #self.X.dispose()
+
+    def testCase(self):
+        print("I am a test case, big and stronk2")
+
+class Asteroird(unittest.TestCase):
+    pass
+
+
+class Basher(unittest.TestCase):
+    pass
+
+
+class Camera(unittest.TestCase):
+    pass
+
+
+class Circ(unittest.TestCase):
+    pass
+
+
+class DeflectorShield(unittest.TestCase):
+    pass
+
+
+class Enemy(unittest.TestCase):
+    pass
+
+
+class EnemyBullet(unittest.TestCase):
+    pass
+
+
+# Should be a good place to put alot of unit tests
+class ezmath(unittest.TestCase):
+    pass
+
+
+class gameFunctions(unittest.TestCase):
+    pass
+
+
+class GlobalVariables(unittest.TestCase):
+    pass
+
+
+# cant really do anything here
+class Img(unittest.TestCase):
+    pass
+
+
+class IonBullet(unittest.TestCase):
+    pass
+
+
+class IonCannon(unittest.TestCase):
+    pass
+
+
+class Item(unittest.TestCase):
+    pass
+
+
+class Missile(unittest.TestCase):
+    pass
+
+
+class MissileLauncher(unittest.TestCase):
+    pass
+
+
+class MotherCow(unittest.TestCase):
+    pass
+
+
+class MotherCowDeath(unittest.TestCase):
+    pass
+
+
+class OverShield(unittest.TestCase):
+    pass
+
+
+class Particle(unittest.TestCase):
+    pass
+
+
+class Player(unittest.TestCase):
+    pass
+
+
+class Poly(unittest.TestCase):
+    pass
+
+
+class Projectile(unittest.TestCase):
+    pass
+
+
+class QuadShooter(unittest.TestCase):
+    pass
+
+
+class RapidGun(unittest.TestCase):
+    pass
+
+
+class Shaper(unittest.TestCase):
+    pass
+
+
+class SpaceGame(unittest.TestCase):
+    pass
+
+
+class SpreadGun(unittest.TestCase):
+    pass
+
+
+class Weapon(unittest.TestCase):
+    pass
--- UnitTests/TestMain.py
+++ UnitTests/TestMain.py
@@ -2,128 +2,7 @@ import unittest
 import sys
 import subprocess
 
-
-class Alien(unittest.TestCase):
-    pass
-
-
-class Asteroird(unittest.TestCase):
-    pass
-
-
-class Basher(unittest.TestCase):
-    pass
-
-
-class Camera(unittest.TestCase):
-    pass
-
-
-class Circ(unittest.TestCase):
-    pass
-
-
-class DeflectorShield(unittest.TestCase):
-    pass
-
-
-class Enemy(unittest.TestCase):
-    pass
-
-
-class EnemyBullet(unittest.TestCase):
-    pass
-
-
-# Should be a good place to put alot of unit tests
-class ezmath(unittest.TestCase):
-    pass
-
-
-class gameFunctions(unittest.TestCase):
-    pass
-
-
-class GlobalVariables(unittest.TestCase):
-    pass
-
-
-# cant really do anything here
-class Img(unittest.TestCase):
-    pass
-
-
-class IonBullet(unittest.TestCase):
-    pass
-
-
-class IonCannon(unittest.TestCase):
-    pass
-
-
-class Item(unittest.TestCase):
-    pass
-
-
-class Missile(unittest.TestCase):
-    pass
-
-
-class MissileLauncher(unittest.TestCase):
-    pass
-
-
-class MotherCow(unittest.TestCase):
-    pass
-
-
-class MotherCowDeath(unittest.TestCase):
-    pass
-
-
-class OverShield(unittest.TestCase):
-    pass
-
-
-class Particle(unittest.TestCase):
-    pass
-
-
-class Player(unittest.TestCase):
-    pass
-
-
-class Poly(unittest.TestCase):
-    pass
-
-
-class Projectile(unittest.TestCase):
-    pass
-
-
-class QuadShooter(unittest.TestCase):
-    pass
-
-
-class RapidGun(unittest.TestCase):
-    pass
-
-
-class Shaper(unittest.TestCase):
-    pass
-
-
-class SpaceGame(unittest.TestCase):
-    pass
-
-
-class SpreadGun(unittest.TestCase):
-    pass
-
-
-class Weapon(unittest.TestCase):
-    pass
-
+from Classes import testClasses as testClass
 
 class ParticleSuper(unittest.TestCase):
     def setUp(self):
@@ -137,18 +16,6 @@ class ParticleSuper(unittest.TestCase):
     def testCase(self):
         print("I am a test case, big and stronk1")
 
-class EnemySuper(unittest.TestCase):
-    def setUp(self):
-        print("Setup\n")
-        #self.X = X
-
-    def tearDown(self):
-        print("Takedown\n")
-        #self.X.dispose()
-
-    def testCase(self):
-        print("I am a test case, big and stronk2")
-
 # class ParticleTestPositive(ParticleSuper):
 #    def runTest(self):
 #        print("Start test\n")
@@ -160,7 +27,7 @@ def suite():
     suiteClasses = unittest.TestSuite()
 
     suiteClasses.addTest(ParticleSuper("testCase"))
-    suiteGeneral.addTest(EnemySuper("testCase"))
+    suiteGeneral.addTest(testClass.Alien("testCase"))
 
     suite = unittest.TestSuite((suiteGeneral, suiteClasses))
 
