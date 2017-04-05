a740c8d82155aa260d3a2bc7e4a7e0216ea47c32
Reid <reid.vollett@uoit.net>
2017-04-04 21:08:21 -0400

    remove spacegame

--- UnitTests/Classes/testClasses.py
+++ UnitTests/Classes/testClasses.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import Alien, Asteroid, Basher, Camera, Circ, DeflectorShield, Enemy, EnemyBullet, ezmath, gameFunctions, \
     GlobalVariables, IonBullet, IonCannon, Item, Missile, MissileLauncher, MotherCow, MotherCowDeath, OverShield, \
-    Particle, Player, Poly, Projectile, QuadShooter, RapidGun, Shape, SpreadGun, Weapon, SpaceGame
+    Particle, Player, Poly, Projectile, QuadShooter, RapidGun, Shape, SpreadGun, Weapon
 
 class AlienTest(unittest.TestCase):
     def setUp(self):
